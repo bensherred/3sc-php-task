56f3355cdf662b4fd1925a97b592514d856f5793
Ben Sherred <ben@sherred.co.uk>
2020-11-25 12:08:32 +0000

    Add the initial local filesystem adapter

---
+++ src/Filesystem/Adapters/LocalAdapter.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Tsc\CatStorageSystem\Filesystem\Adapters;
+
+use DirectoryIterator;
+use SplFileInfo;
+use Tsc\CatStorageSystem\Contracts\AdapterInterface;
+
+class LocalAdapter implements AdapterInterface
+{
+    /**
+     * Get all the files within the specified directory.
+     *
+     * @param  string  $path
+     * @return SplFileInfo[]
+     */
+    public function files(string $path): array
+    {
+        $files = [];
+        $filesIterator = new DirectoryIterator($path);
+
+        foreach ($filesIterator as $item) {
+            if ($item->isDir() || $item->isDot()) {
+                continue;
+            }
+
+            $files[] = $item->getFileInfo();
+        }
+
+        return $files;
+    }
+
+    /**
+     * Create a file in the specified directory.
+     *
+     * @param  string  $path
+     * @param  string  $name
+     * @return SplFileInfo
+     */
+    public function makeFile(string $path, string $name): SplFileInfo
+    {
+        $fullPath = $path . '/' . $name;
+
+        touch($fullPath);
+
+        return new SplFileInfo($fullPath);
+    }
+
+    /**
+     * Update the specified file.
+     *
+     * @param  string  $path
+     * @param  int  $time
+     * @return SplFileInfo
+     */
+    public function updateFile(string $path, int $time): SplFileInfo
+    {
+        touch($path, $time);
+
+        // Clear the file status cache due to modify mt time
+        clearstatcache();
+
+        return new SplFileInfo($path);
+    }
+
+    /**
+     * Rename the specified file.
+     *
+     * @param  string  $path
+     * @param  string  $oldName
+     * @param  string  $newName
+     * @return SplFileInfo
+     */
+    public function renameFile(string $path, string $oldName, string $newName): SplFileInfo
+    {
+        $oldPath = $path . '/' . $oldName;
+        $newPath = $path . '/' . $newName;
+
+        rename($oldPath, $newPath);
+
+        return new SplFileInfo($newPath);
+    }
+
+    /**
+     * Delete the specified file.
+     *
+     * @param  string  $path
+     * @return bool
+     */
+    public function deleteFile(string $path): bool
+    {
+        return unlink($path);
+    }
+
+    /**
+     * Get a list of directories within the parent directory.
+     *
+     * @param  string  $path
+     * @return SplFileInfo[]
+     */
+    public function directories(string $path): array
+    {
+        $directories = [];
+        $directoryIterator = new DirectoryIterator($path);
+
+        foreach ($directoryIterator as $item) {
+            if (! $item->isDir() || $item->isDot()) {
+                continue;
+            }
+
+            $directories[] = $item->getFileInfo();
+        }
+
+        return $directories;
+    }
+
+    /**
+     * Get the size of the specified directory.
+     *
+     * @param  string  $path
+     * @return int
+     */
+    public function directorySize(string $path): int
+    {
+        return (new SplFileInfo($path))->getSize();
+    }
+
+    /**
+     * Make the specified directory.
+     *
+     * @param  string  $path
+     * @return SplFileInfo
+     */
+    public function makeDirectory(string $path): SplFileInfo
+    {
+        mkdir($path);
+
+        return new SplFileInfo($path);
+    }
+
+    /**
+     * Rename the specified directory.
+     *
+     * @param  string  $path
+     * @param  string  $oldName
+     * @param  string  $newName
+     * @return SplFileInfo
+     */
+    public function renameDirectory(string $path, string $oldName, string $newName): SplFileInfo
+    {
+        $oldPath = $path . '/' . $oldName;
+        $newPath = $path . '/' . $newName;
+
+        rename($oldPath, $newPath);
+
+        return new SplFileInfo($newPath);
+    }
+
+    /**
+     * Delete the specified directory.
+     *
+     * @param  string  $path
+     * @return bool
+     */
+    public function deleteDirectory(string $path): bool
+    {
+        return rmdir($path);
+    }
+}
---
+++ tests/Filesystem/Adapters/LocalAdapterTest.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Tsc\CatStorageSystem\Tests\Filesystem\Adapters;
+
+use DateInterval;
+use DateTime;
+use SplFileInfo;
+use SplFileObject;
+use Tsc\CatStorageSystem\Contracts\AdapterInterface;
+use Tsc\CatStorageSystem\Filesystem\Adapters\LocalAdapter;
+use Tsc\CatStorageSystem\Tests\TestCase;
+
+class LocalAdapterTest extends TestCase
+{
+    protected AdapterInterface $adapter;
+
+    protected string $path;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->adapter = new LocalAdapter();
+        $this->path = './testing-tmp';
+    }
+
+    public function test_it_can_get_all_the_files_in_a_directory()
+    {
+        $directoryName = $this->faker->word;
+        $path = $this->path . '/' . $directoryName;
+
+        mkdir($path);
+
+        $fileName = $this->faker->word . '.txt';
+
+        $this->assertIsArray($this->adapter->files($path));
+        $this->assertEmpty($this->adapter->files($path));
+
+        touch($path . '/' . $fileName);
+
+        $files = $this->adapter->files($path);
+
+        $this->assertCount(1, $files);
+        $this->assertSame($fileName, $files[0]->getFilename());
+    }
+
+    public function test_it_can_make_a_file()
+    {
+        $fileName = $this->faker->word . '.txt';
+        $path = $this->path . '/' . $fileName;
+
+        $this->assertFileDoesNotExist($path);
+
+        $this->adapter->makeFile($this->path, $fileName);
+
+        $this->assertFileExists($path);
+    }
+
+    public function test_it_can_update_a_file()
+    {
+        $fileName = $this->faker->word . '.txt';
+        $path = $this->path . '/' . $fileName;
+
+        touch($path);
+
+        $newTime = (new DateTime())
+            ->add(new DateInterval('P1W'))
+            ->getTimestamp();
+
+        $this->adapter->updateFile($path, $newTime);
+
+        $modifiedTime = (new SplFileInfo($path))->getMTime();
+
+        $this->assertSame($newTime, $modifiedTime);
+    }
+
+    public function test_it_can_rename_a_file()
+    {
+        $oldName = $this->faker->word . '.txt';
+        touch($this->path . '/' . $oldName);
+
+        $this->assertFileExists($this->path . '/' . $oldName);
+
+        $newName = $this->faker->word . '.txt';
+
+        $this->adapter->renameFile($this->path, $oldName, $newName);
+
+        $this->assertFileExists($this->path . '/' . $newName);
+        $this->assertFileDoesNotExist($this->path . '/' . $oldName);
+    }
+
+    public function test_it_can_delete_a_file()
+    {
+        $fileName = $this->faker->word . '.txt';
+        $path = $this->path . '/' . $fileName;
+
+        touch($path);
+
+        $this->assertFileExists($path);
+
+        $this->adapter->deleteFile($path);
+
+        $this->assertFileDoesNotExist($path);
+    }
+
+    public function test_it_can_get_a_list_of_directories()
+    {
+        $this->assertIsArray($this->adapter->directories($this->path));
+        $this->assertEmpty($this->adapter->directories($this->path));
+
+        $directoryName = $this->faker->word;
+        mkdir($this->path . '/' . $directoryName);
+
+        $directories = $this->adapter->directories($this->path);
+
+        $this->assertCount(1, $directories);
+        $this->assertSame($directoryName, $directories[0]->getFilename());
+    }
+
+    public function test_it_can_get_the_size_of_a_directory()
+    {
+        $fileName = $this->faker->word . '.txt';
+        $path = $this->path . '/' . $fileName;
+
+        $size = (new SplFileObject($path, 'w'))
+            ->fwrite('3 Sided Cube');
+
+        $this->assertIsInt($this->adapter->directorySize($path));
+        $this->assertSame($size, $this->adapter->directorySize($path));
+    }
+
+    public function test_it_can_make_a_directory()
+    {
+        $directoryName = $this->faker->word;
+        $path = $this->path . '/' . $directoryName;
+
+        $this->assertDirectoryDoesNotExist($path);
+
+        $this->adapter->makeDirectory($path);
+
+        $this->assertDirectoryExists($path);
+    }
+
+    public function test_it_can_rename_a_directory()
+    {
+        $directoryName = $this->faker->word;
+        $path = $this->path . '/' . $directoryName;
+
+        mkdir($path);
+
+        $this->assertDirectoryExists($path);
+
+        $newName = $this->faker->word;
+        $this->adapter->renameDirectory($this->path, $directoryName, $newName);
+
+        $this->assertDirectoryExists($this->path . '/' . $newName);
+        $this->assertDirectoryDoesNotExist($path);
+    }
+
+    public function test_it_can_delete_a_directory()
+    {
+        $directoryName = $this->faker->word;
+        $path = $this->path . '/' . $directoryName;
+
+        mkdir($path);
+
+        $this->assertDirectoryExists($path);
+
+        $this->adapter->deleteDirectory($path);
+
+        $this->assertDirectoryDoesNotExist($path);
+    }
+}
